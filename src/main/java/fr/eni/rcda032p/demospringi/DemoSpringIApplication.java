1d4a07b2656f4b660d92485dfeedddc73fba72b0
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2024-01-15 16:51:51 +0100

    init

---
+++ src/main/java/fr/eni/rcda032p/demospringi/DemoSpringIApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.rcda032p.demospringi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/rcda032p/demospringi/DemoSpringIApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.rcda032p.demospringi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
